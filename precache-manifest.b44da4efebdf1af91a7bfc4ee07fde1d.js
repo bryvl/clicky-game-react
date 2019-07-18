0ec5aee657f9c438d1a12f850e703bb46b7a424b
bryvl <bryan.val93@gmail.com>
2019-07-17 21:47:24 -0400

    Updates

---
+++ precache-manifest.b44da4efebdf1af91a7bfc4ee07fde1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7f1d752bfc489da20aff2de8583d54",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "5e67e4a8429aba9c5a28",
+    "url": "/clicky-game-react/static/css/main.a81f7f62.chunk.css"
+  },
+  {
+    "revision": "938de5c6f9ce460c699f",
+    "url": "/clicky-game-react/static/js/2.78c5a442.chunk.js"
+  },
+  {
+    "revision": "5e67e4a8429aba9c5a28",
+    "url": "/clicky-game-react/static/js/main.468f4ec0.chunk.js"
+  },
+  {
+    "revision": "96e9a9b550dae971e8fe",
+    "url": "/clicky-game-react/static/js/runtime~main.4ef103c0.js"
+  }
+]);
